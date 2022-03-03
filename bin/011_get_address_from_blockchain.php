4cf63eb30c776b33dd87737ca4e03f604393ac37
liksagen.com <github@defihuntersdao.club>
2022-03-03 18:19:13 +0000

    pre beta

--- bin/011_get_address_from_blockchain.php
+++ bin/011_get_address_from_blockchain.php
@@ -9,16 +9,21 @@ $time = time();
 
 print_r($argv);
 
+
 switch($argv[1])
 {
     case "test":
         $contractAddress = "0xb50c22c740D5a034023459bAccD3c785C6bB8303";
-	$tkn = "0x47a1162C73b565c7F0a5bD16168e3B2cA38942D5";
+	$tkn 	= "0x47a1162C73b565c7F0a5bD16168e3B2cA38942D5";
+	$tkn2	= "0x0EEAfFCfeA437f5822Fc1537b95Fc84EEBb3D9Db";
+	
     break;
     default:
-    $contractAddress = "0xa9a2d6b16f3dd4c145aa8c875b9ceb8cda3022e3";
-    $tkn = "0x90F3edc7D5298918F7BB51694134b07356F7d0C7";
+	$contractAddress = "0xa9a2d6b16f3dd4c145aa8c875b9ceb8cda3022e3";
+        $tkn 	= "0x90F3edc7D5298918F7BB51694134b07356F7d0C7";
+	$tkn2 	= "0xCA1931C970CA8C225A3401Bb472b52C46bBa8382";
 }
+
 
 $n_mas[0] = "ddao_seed_";
 $n_mas[1] = "ddao_private1_";
@@ -52,6 +57,7 @@ for($i=0;$i<3;$i++)
 unset($t);
 //$t[from] = $wal;
 //$b = "0xdd3680fc";
+/*
 $b = "0x6c75356b";
 //0x70a08231000000000000000000000000b2207c34de61f3018576cb637fa90dae0425d916
 $t2 = substr($w,2);
@@ -63,6 +69,16 @@ $t2 = view_number($t2,64,"0");
 $b .= $t2;
 
 $t2 = 0; 
+$t2 = view_number($t2,64,"0");
+$b .= $t2;
+*/
+
+$b = "0x675b6ac9";
+$t2 = substr($w,2);
+$t2 = view_number($t2,64,"0");
+$b .= $t2;
+
+$t2 = $i; 
 $t2 = view_number($t2,64,"0");
 $b .= $t2;
 
@@ -141,6 +157,42 @@ $v[params][1] = "latest";
 $v[id] = $w."-"."ddao_balance";
 $jss[] = $v;
 
+//------------------------------------------------
+unset($t);
+//$t[from] = $wal;
+//$b = "0xdd3680fc";
+$b = "0xdd62ed3e";
+//0x70a08231000000000000000000000000b2207c34de61f3018576cb637fa90dae0425d916
+/*
+$t2 = $i; 
+$t2 = view_number($t2,64,"0");
+$b .= $t2;
+*/
+
+$t2 = substr($w,2);
+$t2 = view_number($t2,64,"0");
+$b .= $t2;
+
+$t2 = substr($contractAddress,2);
+$t2 = view_number($t2,64,"0");
+$b .= $t2;
+
+
+$t[data] = $b;
+$t[to] = $tkn2;
+//print_r($t);
+
+$v[jsonrpc] = "2.0";
+$v[method] = "eth_call";
+//$v[params][0] = $row[wal];
+$v[params][0] = $t;
+$v[params][1] = "latest";
+//$v[id] = $row[id];
+//$v[id] = $net."_"."AllocSaleAmount_".$sale_id;
+$v[id] = $w."-"."addao_allowance";
+$jss[] = $v;
+
+
 
 }
 
@@ -174,9 +226,22 @@ foreach($mas as $v2)
     $t = gmp_hexdec($t);
     $t = gmp_div($t,10**18);
     $t = gmp_strval($t);
+
+    switch($k)
+    {
+	case "ddao_balance":
+	    if($t>100000)$t = "100k+";
+	break;
+	default:
+    }
     $o[$w][$k] = $t;
 
+$o[$w][addr_contract] = $contractAddress;
+$o[$w][addr_addao] = $tkn2;
+$o[$w][time] = date("Y-m-d H:i:s");
+
 }
+
 print_r($o);
 foreach($o as $w=>$v2)
 {
