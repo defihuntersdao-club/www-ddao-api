a945e0028fbd1af3aa75be03c3dba70af8d3590d
liksagen.com <github@defihuntersdao.club>
2022-07-05 17:46:24 +0000

    change tkn address

--- bin/018_conf.php
+++ bin/018_conf.php
@@ -18,6 +18,8 @@ $c[bsc] = "0x08Ff8B625AbfB21452b3fb284a85735cAB6fb9f3";
                                                                                                                                                                                             
 $tkns[matic] = "0x19Ca9521Ec3F01a03476323dd54740C1239eF5e5";                                                                                                                                
 $tkns[bsc] = "0xd4987eBA16672165B14f50C84fC4AEb8Dd986772";                                                                                                                                  
+$tkns[matic] = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+$tkns[bsc] = "0x55d398326f99059ff775485246999027b3197955";
 
 $decimal[bsc] = 18;
 $decimal[matic] = 6;
